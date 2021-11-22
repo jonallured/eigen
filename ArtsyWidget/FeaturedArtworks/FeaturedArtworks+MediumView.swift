61548421aac435ea9d32a8a8a1002dab94f45084
Jon Allured <jon.allured@gmail.com>
2021-11-22 16:43:21 -0600

    WIP

---
+++ ArtsyWidget/FeaturedArtworks/FeaturedArtworks+MediumView.swift
@@ -0,0 +1,21 @@
+//
+//  FeaturedArtworks+MediumView.swift
+//  ArtsyWidgetExtension
+//
+//  Created by Jonathan Allured on 11/22/21.
+//  Copyright © 2021 Artsy. All rights reserved.
+//
+
+import SwiftUI
+
+struct FeaturedArtworks_MediumView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FeaturedArtworks_MediumView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeaturedArtworks_MediumView()
+    }
+}
---
+++ ArtsyWidget/FeaturedArtworks/FeaturedArtworks+SmallView.swift
@@ -0,0 +1,21 @@
+//
+//  FeaturedArtworks+SmallView.swift
+//  ArtsyWidgetExtension
+//
+//  Created by Jonathan Allured on 11/22/21.
+//  Copyright © 2021 Artsy. All rights reserved.
+//
+
+import SwiftUI
+
+struct FeaturedArtworks_SmallView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FeaturedArtworks_SmallView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeaturedArtworks_SmallView()
+    }
+}
